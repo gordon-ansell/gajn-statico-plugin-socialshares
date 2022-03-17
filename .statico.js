bfe7dd6ad9413a0178e61dabc13862ab5ae52e1e
GordonAnsell <moving@ga1964.com>
2022-03-17 05:57:17 +0000

    Update .statico.js

--- .statico.js
+++ .statico.js
@@ -7,6 +7,7 @@
 'use strict';
 
 const { syslog } = require('js-framework');
+const pack = require('./package.json');
 const SocialSharesShortcode = require('./src/shortcodes/socialsharesShortcode');
 const debug = require('debug')('Statico:plugin:socialshares'),
       debugf = require('debug')('Full.Statico:plugin:socialshares');
@@ -16,6 +17,8 @@ module.exports = function(config, options = {}) {
 
     config.addNunjucksShortcode('socialshares', SocialSharesShortcode);
     debug(`Added shortcode to Nunjucks: socialshares`);
+
+    syslog.notice(`Statico social shares plugin version ${pack.version} loaded.`);
 
 }
  
